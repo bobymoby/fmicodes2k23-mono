b35d827940c53cb03a9bf3f0c0eb7b040c480503
DariaLaz <daria.lazarova03@gmail.com>
2023-03-17 22:59:41 +0200

    lobby

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,21 @@ import logo from './logo.svg'
 import './App.scss'
 import Login from './components/Login'
 import Register from './components/Register'
+import { Lobby } from './components/Lobby'
 
 function App() {
     return (
         <div className="App">
-            <Login/>
+            <Lobby 
+                code='1234'
+                players={
+                    [
+                        {name:"name1", image:"../logo.svg"},
+                        {name:"name2", image:"../logo.svg"},
+                        {name:"name3", image:"../logo.svg"},
+                        {name:"name4", image:"../logo.svg"},
+                    ]
+                }/>
         </div>
     )
 }
