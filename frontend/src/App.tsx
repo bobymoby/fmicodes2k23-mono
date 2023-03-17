5f32ef74a58a6ff90d95cadc1d15c996b6b5f98b
Pavel035 <pavel.kosanov@gmail.com>
2023-03-17 23:11:57 +0200

    Added join
    Join functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg'
 import './App.scss'
 import Login from './components/Login'
 import Register from './components/Register'
+import Join from './components/Join'
 
 function App() {
     return (
         <div className="App">
-            <Login/>
+            <Join/>
         </div>
     )
 }
