d6435cf433d2fc8220e866181e7c4b2d48c61d9a
TheHotDoge <47058577+TheHotDoge@users.noreply.github.com>
2023-03-18 02:38:37 +0200

    test 1 integration

---
+++ tests/1/integration.sql
@@ -0,0 +1,17 @@
+INSERT INTO 'task' ('title', 'description')
+VALUES ('Task 1', ' write a function which returns the sum of the first and last element of an array')
+
+INSERT INTO 'test' ('input', 'output')
+VALUES ('1
+3
+4
+7
+20', '21')
+
+INSERT INTO 'test' ('input', 'output')
+VALUES ('5
+3
+-12
+14
+23
+-5', '0')
--- tests/1/migration.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO 'task' ('title', 'description')
-VALUES ('title', 'description')
-
-INSERT INTO 'test' ('input', 'output')
-VALUES ('1\n3\n4\n7\n20', '20')
