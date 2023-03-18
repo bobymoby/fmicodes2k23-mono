7094dfd92b30d3b0d260712a49e5aed98a4e072e
TheHotDoge <47058577+TheHotDoge@users.noreply.github.com>
2023-03-18 15:27:03 +0200

    Summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { Route, Routes } from 'react-router'
 import { RegisterPage } from './Pages/AuthPages/RegisterPage'
 import { POCEvent } from './Pages/POCEvent/POCEvent'
+import { RoomPage } from './Pages/RoomPage/RoomPage'
+import { LobbyPage } from './Pages/LobbyPage/LobbyPage'
+import { VotePage } from './Pages/VotePage/VotePage'
 
 function App() {
     return (
@@ -12,6 +15,7 @@ function App() {
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/reg" element={<RegisterPage />} />
                 <Route path="/poc" element={<POCEvent />} />
+                <Route path="/room" element={<RoomPage tasks={[{task:"task1", userId:"123", name:"task1", id:"1"}]}/>} />
             </Routes>
         </BrowserRouter>
     )
