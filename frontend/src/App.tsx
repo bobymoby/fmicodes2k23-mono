3010088fb4562ecf9b64d8449b6a93269b49d82e
Боримир Георгиев <imbobimobi@gmail.com>
2023-03-18 14:52:20 +0200

    frontend websockets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import './App.scss'
 import { LoginPage } from './Pages/AuthPages/LoginPage'
-import { EndGamePage } from './Pages/EndGamePage/EndGamePage'
-import { RoomPage } from './Pages/RoomPage/RoomPage'
 import { BrowserRouter } from 'react-router-dom'
 import { Route, Routes } from 'react-router'
 import { RegisterPage } from './Pages/AuthPages/RegisterPage'
 import { POCEvent } from './Pages/POCEvent/POCEvent'
-import { socket } from './utils/socket'
-import { LobbyPage } from './Pages/LobbyPage/LobbyPage'
-import Room from './components/Room'
-import { RulesPage } from './Pages/RulesPage/RulesPage'
-import { VotePage } from './Pages/VotePage/VotePage'
 
 function App() {
     return (
--- frontend/src/Pages/POCEvent/POCEvent.tsx
+++ frontend/src/Pages/POCEvent/POCEvent.tsx
@@ -3,22 +3,80 @@ import { socket } from '../../utils/socket'
 
 export const POCEvent = () => {
     const [tasks, setTasks] = useState([])
-    socket.emit('getGameDetails', { roomId: 1 })
-    socket.on('gameDetails', (data) => {
-        setTasks(data.tasks)
+    const [selectedTask, setSelectedTask] = useState<any>(null)
+    const gameId = '3a926831-5c2b-4f65-b90e-eb79b94001cc'
+    // const userId = 'd8a9086f-1108-4c8c-991c-f34e13f0768a'
+    const [userId, setUserId] = useState('')
+    const handleUserChange = (event: any) => {
+        setUserId(event.target.value)
+    }
+    const handleConnect = () => {
+        socket.emit('getGameDetails', {
+            userId,
+        })
+    }
+    socket.on('gameState/' + gameId, (data) => {
+        setTasks(data.game.tasks)
     })
+
+    const selectTask = async (task: any) => {
+        fetch('http://localhost:3000/user/' + userId, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ taskId: task.id }),
+        }).then((res) => {
+            if (res.ok) {
+                setSelectedTask(task)
+                socket.emit('getGameDetails', {
+                    userId,
+                })
+            }
+        })
+    }
+
+    const handleCodeChange = (event: any) => {
+        const newTask = { ...selectedTask, code: event.target.value }
+        setSelectedTask(newTask)
+        fetch('http://localhost:3000/task/' + selectedTask.id, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code: newTask.code }),
+        })
+        // .then((res) => {
+        //     socket.emit('getGameDetails', {
+        //         userId,
+        //     })
+        // })
+        // .catch((err) => console.log(err))
+    }
     return (
         <div>
             <p>POC</p>
+            <input type="text" value={userId} onChange={handleUserChange} />
+            <button onClick={handleConnect}>connect</button>
             {tasks.map((task: any) => {
                 return (
-                    <div>
-                        <p>
-                            {task.name} {task.open ? 'open' : 'closed'}
-                        </p>
+                    <div onClick={() => selectTask(task)}>
+                        {task.title}{' '}
+                        {task.openedBy
+                            ? 'opened by ' + task.openedBy.username
+                            : 'not open'}
                     </div>
                 )
             })}
+            {selectedTask && (
+                <div>
+                    <p>selected task {selectedTask.title}</p>
+                    <textarea
+                        value={selectedTask.code || ''}
+                        onChange={handleCodeChange}
+                    />
+                </div>
+            )}
         </div>
     )
 }
